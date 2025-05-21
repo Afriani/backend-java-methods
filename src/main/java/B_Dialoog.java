df7bc9494713ca91191c4cbb6e27f7ca5d918c7e
Afriani Sinaga <sinaga.afriani88@gmail.com>
2025-05-21 18:28:53 +0200

    my first backend project

--- src/main/java/B_Dialoog.java
+++ src/main/java/B_Dialoog.java
@@ -2,10 +2,7 @@ public class B_Dialoog {
 
     public static void main(String[] args) {
         // In deze dialoog praten Bert en Ernie met elkaar.
-        // Maak methode bertSays die "Bert says: " + de string print en een methode ernieSays die de string " says Ernie" print.
-        // De methode heeft als parameter de tekst die gesproken moet worden.
-        // De volgende zin wordt steeds door de ander gesproken.
-        // Vervang System.out.println() steeds door je eigen method.
+
         System.out.println("Hey"); //Bert
         System.out.println("Hey"); //Ernie
         System.out.println("Welcome to this wonderful conversation");
@@ -16,7 +13,29 @@ public class B_Dialoog {
         System.out.println("Well... I did watch a random program on a random channel...");
         System.out.println("Thank you for a wonderful conversation!");
         System.out.println("Cya");
+
+        // Maak methode bertSays die "Bert says: " + de string print en een methode ernieSays die de string " says Ernie" print.
+        // De methode heeft als parameter de tekst die gesproken moet worden.
+        // De volgende zin wordt steeds door de ander gesproken.
+        // Vervang System.out.println() steeds door je eigen method.
+        bertSays("Hey"); //Bert
+        ernieSays("Hey"); //Ernie
+        bertSays("Welcome to this wonderful conversation");
+        ernieSays("Thnx, I'm looking forward to it");
+        bertSays("How about this weather?");
+        ernieSays("I really don't mind the cold....");
+        bertSays("And did you see that random program on that random channel?");
+        ernieSays("Well... I did watch a random program on a random channel...");
+        bertSays("Thank you for a wonderful conversation!");
+        ernieSays("Cya");
     }
 
     // nieuwe methods komen hier
+    public static void bertSays(String tekst) {
+        System.out.println("Bert says: " + tekst);
+    }
+
+    public static void ernieSays(String tekst) {
+        System.out.println(tekst + " says Ernie");
+    }
 }
--- src/main/java/C_Oppervlakte.java
+++ src/main/java/C_Oppervlakte.java
@@ -1,22 +1,37 @@
 public class C_Oppervlakte {
 
     public static void main(String[] args) {
-        int antwoord = 0;
+        //int antwoord = 0;
+        int antwoord;
         // Uncomment de regels en maak een method die voor het juiste antwoord zorgt.
 
-        //antwoord = oppervlakteRechthoek(2, 8);
+        antwoord = oppervlakteRechthoek(2, 8);
         System.out.println("De oppervlakte van een rechthoek met lengte 2 en breedte 8 is " + antwoord);
 
-        //antwoord = oppervlakteVierkant(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
+        antwoord = oppervlakteVierkant(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
         System.out.println("De oppervlakte van een vierkant met zijden van 5 cm is " + antwoord);
 
-        //antwoord = oppervlakteDoos(2,8,5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
+        antwoord = oppervlakteDoos(2,8,5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
         System.out.println("De oppervlakte van een doos met lengte 2, breedte 8 en hoogte 5 is " + antwoord);
 
-        //antwoord = oppervlakteKubus(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteVierkant()` hergebruiken, of `oppervlakteDoos()` hergebruiken.
+        antwoord = oppervlakteKubus(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteVierkant()` hergebruiken, of `oppervlakteDoos()` hergebruiken.
         System.out.println("De oppervlakte van een kubus met zijden van 5 cm is " + antwoord);
     }
 
     // nieuwe methods komen hier
+    public static int oppervlakteRechthoek(int lengte, int breedte) {
+        return lengte * breedte;
+    }
 
+    public static int oppervlakteVierkant(int zijde) {
+        return oppervlakteRechthoek(zijde, zijde);
+    }
+
+    public static int oppervlakteDoos(int lengte, int breedte, int hoogte) {
+        return 2 * (lengte * breedte + lengte * hoogte + breedte * hoogte);
+    }
+
+    public static int oppervlakteKubus(int zijde) {
+        return 6 * oppervlakteVierkant(zijde);
+    }
 }
